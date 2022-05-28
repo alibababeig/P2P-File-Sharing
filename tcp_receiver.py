523a9cdb96d8b8abf6e09dfb8ec74f8a18a1e82c
alibababeig <alibababeig@yahoo.com>
2022-05-28 15:00:19 +0430

    protocol headers are added

--- tcp_receiver.py
+++ tcp_receiver.py
@@ -1,33 +1,49 @@
 import socket
 
-EOT = b'\x04'
+FILENAME_LENGTH_BYTES = 2
+PAYLOAD_LENGTH_BYTES = 8
+ENDIANNESS = 'little'
 
 if __name__ == '__main__':
-    file_path = './repository/rx_lake.jpg'
+    repository = './repository/rx/'
     chunk_size = 1000
-    port = 12345
+    my_port = 12345
     backlog = 1
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('', port))
+    sock.bind(('', my_port))
     sock.listen(backlog)
 
-    try:
-        client, clientInfo = sock.accept()
-        f = open(file_path, 'wb')
-        while True:
-            data = client.recv(chunk_size)
-            if data:
-                if data[-1:] == EOT:
-                    f.write(data[:-1])
-                    break
-                else:
-                    f.write(data)
+    client, clientInfo = sock.accept()
+    buffer = b''
+    while len(buffer) < PAYLOAD_LENGTH_BYTES + FILENAME_LENGTH_BYTES:
+        buffer += client.recv(chunk_size)
 
-        client.close()
-        sock.close()
-        f.close()
-    except:
-        print("Closing socket")
-        client.close()
-        sock.close()
+    filename_length = int.from_bytes(
+        buffer[:FILENAME_LENGTH_BYTES], ENDIANNESS)
+    print(filename_length)
+
+    payload_length = int.from_bytes(
+        buffer[FILENAME_LENGTH_BYTES:
+               FILENAME_LENGTH_BYTES + PAYLOAD_LENGTH_BYTES], ENDIANNESS)
+    print(payload_length)
+
+    buffer = buffer[FILENAME_LENGTH_BYTES + PAYLOAD_LENGTH_BYTES:]
+    while len(buffer) < filename_length:
+        buffer += client.recv(chunk_size)
+
+    filename = buffer[:filename_length].decode('utf-8')
+    print(filename)
+
+    f = open(repository + filename, 'wb')
+    written_bytes = 0
+
+    buffer = buffer[filename_length:]
+    while written_bytes < payload_length:
+        f.write(buffer)
+        written_bytes += len(buffer)
+        buffer = client.recv(chunk_size)
+
+    client.close()
+    sock.close()
+    f.close()
--- tcp_sender.py
+++ tcp_sender.py
@@ -1,23 +1,36 @@
 import socket
 from chunk_manager.file_chunker import FileChunker
 
-EOT = b'\x04'
+FILENAME_LENGTH_BYTES = 2
+PAYLOAD_LENGTH_BYTES = 8
+ENDIANNESS = 'little'
 
 if __name__ == '__main__':
-    file_path = './repository/tx_lake.jpg'
+    filename = 'lake.jpg'
+    file_path = f'./repository/tx/{filename}'
     chunk_size = 1000
-    targetIp = '192.168.1.111'
-    port = 12345
+    target_ip = '127.0.0.1'
+    target_port = 12345
 
     file_chunker = FileChunker(file_path, chunk_size)
+
+    filename_length = len(filename.encode(
+        'utf-8')).to_bytes(FILENAME_LENGTH_BYTES, ENDIANNESS)
+
+    payload_length = file_chunker.get_file_size().to_bytes(
+        PAYLOAD_LENGTH_BYTES, ENDIANNESS)
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((targetIp, port))
+    sock.connect((target_ip, target_port))
+
+    sock.send(filename_length)
+    sock.send(payload_length)
+    sock.send(filename.encode("utf-8"))
 
     chunk = file_chunker.get_next_chunk()
     while chunk != None:
         sock.send(chunk)
         chunk = file_chunker.get_next_chunk()
 
-    sock.send(EOT)
     sock.close()
     file_chunker.close_file()
