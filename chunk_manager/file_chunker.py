cd32a5edfbb9340b46afc213fddb8d5991195d5e
alibababeig <alibababeig@yahoo.com>
2022-05-28 14:57:21 +0430

    added method get_file_size

--- chunk_manager/file_chunker.py
+++ chunk_manager/file_chunker.py
@@ -1,7 +1,13 @@
+import os
+
+
 class FileChunker:
     def __init__(self, file_path, chunk_size=1000):
         self.file = open(file_path, 'rb')
         self.chunk_size = chunk_size
+
+    def get_file_size(self):
+        return os.path.getsize(self.file.name)
 
     def get_next_chunk(self):
         chunk = self.file.read(self.chunk_size)
