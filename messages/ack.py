fc89b5fa6e74f2e2829d4317b0cd77ed04eb4660
alibababeig <alibababeig@yahoo.com>
2022-05-29 18:42:24 +0430

    added class Ack for handling ack messages

---
+++ messages/ack.py
@@ -0,0 +1,44 @@
+FILENAME_LENGTH_BYTES = 2
+PORT_NUMBER_BYTES = 2
+ENDIANNESS = 'little'
+
+
+class Ack:
+    def __init__(self):
+        self.data = None
+        self.bytes = None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, filename, port_number):
+        self.data = (filename, port_number)
+
+        filename_length = len(filename.encode('utf-8'))
+        _bytes = filename_length.to_bytes(FILENAME_LENGTH_BYTES, ENDIANNESS)
+        _bytes += filename.encode('utf-8')
+        _bytes += port_number.to_bytes(PORT_NUMBER_BYTES, ENDIANNESS)
+
+        self.bytes = _bytes
+
+    def get_bytes(self):
+        return self.bytes
+
+    def set_bytes(self, _bytes):
+        self.bytes = _bytes
+
+        if len(_bytes) < FILENAME_LENGTH_BYTES:
+            raise ValueError()
+
+        filename_length = int.from_bytes(
+            _bytes[:FILENAME_LENGTH_BYTES], ENDIANNESS)
+        if len(_bytes) != FILENAME_LENGTH_BYTES + filename_length + PORT_NUMBER_BYTES:
+            raise ValueError()
+
+        filename = _bytes[FILENAME_LENGTH_BYTES:
+                          FILENAME_LENGTH_BYTES + filename_length].decode('utf-8')
+
+        port_number = int.from_bytes(
+            _bytes[FILENAME_LENGTH_BYTES + filename_length:], ENDIANNESS)
+
+        self.data = (filename, port_number)
