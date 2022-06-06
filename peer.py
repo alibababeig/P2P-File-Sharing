07c543617c69f09c44ef4dee2bb2774e63cbf5a8
sehbeygi79 <sehbeygi79@gmail.com>
2022-06-06 14:56:02 +0430

    argparser added

--- peer.py
+++ peer.py
@@ -1,6 +1,15 @@
+import argparse
 from ui.p2p_file_sharing import P2PFileSharing
 
 
-peer = P2PFileSharing()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed-only', dest='seed_only', help='co', action='store_true', default=False)
+    args = parser.parse_args()
 
-# peer.request_file()
+    peer = P2PFileSharing()
+    
+    while args.seed_only:
+        query = input('enter your query:')
+        peer.request_file(query)
+        print('successfully done')
