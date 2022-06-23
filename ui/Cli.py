6b40a42df3c210f20404aeaa291fe7a3e3db07a2
sehbeygi79 <sehbeygi79@gmail.com>
2022-06-23 15:22:38 +0430

    changed to use host_id instead of host_ip

--- ui/Cli.py
+++ ui/Cli.py
@@ -56,15 +56,13 @@ class Cli:
               .format('', '', '', 'Offerer Address', 'File Size', 'File Name') + Color.ENDC.value)
         line = 0
         for offerer, matching_files in offers.items():
-            offerer_ip, offerer_port = offerer
-
             for dic in matching_files:
                 filename = dic['name']
                 filesize = Cli.__generate_filesize_string(dic['size'])
                 line += 1
 
                 print(Cli.offer_row
-                      .format(Color.BOLD.value, f'[{line}]', Color.ENDC.value, f'{offerer_ip}:{offerer_port}', filesize, filename))
+                      .format(Color.BOLD.value, f'[{line}]', Color.ENDC.value, offerer, filesize, filename))
 
     def __generate_filesize_string(filesize):
         if filesize < 1000:
